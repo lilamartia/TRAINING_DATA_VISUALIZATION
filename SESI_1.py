c39980e8d56a6c7582c39fdef86e1504b8e2542c
DeshPur <lillamartianingrum@gmail.com>
2022-06-24 13:20:58 +0700

    Initial Commit add Sesi 1

---
+++ SESI_1.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 19 19:35:29 2022
+
+@author: Lenovo
+"""
+
+print(123123+1)
+print(123123 / 3 )
+
+print(10)
+print(type(10))
+
+print(4.2)
+print(type(4.2))
+print(4.)
+print(.2)
+print(.4e7)
+print(4.2e-4)
+
+print("Hacktiv8")
+print(type("Hacktiv8"))
+
+print(type(True))
+
+n = 300
+print(n)
+
+a = 10
+t = 20
+l = (a*t/2)
+print(l) 
+
+a = b = c = 300
+print(a, b, c)
+
+name = "Hacktiv8"
+Age = 54
+has_laptops = True
+print(name, Age, has_laptops)
+
+age = 1
+Age = 2
+aGe = 3
+AGE = 4
+a_g_e = 5
+_age = 6
+age_ = 7
+_AGE_ = 8
+
+print(age, Age, aGe, AGE, a_g_e, _age, age_, _AGE_)
+
+a = 10
+b = 20
+a + b
+print(a + b)
+
+a = 4
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+s = 'foo'
+t = 'bar'
+u = 'baz'
+
+print(s + t)
+
+print(s + t + u)
+
